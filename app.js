0e84a8553301f2f4e64b8bd8ff74ca743c5d1c83
mahtabullahysd23 <mahtab.ullah@bjitacademy.com>
2023-09-15 01:21:28 +0600

    verificaion added
    verification added for user login
    
    refs#65487

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const connectDB = require("./config/database");
+const response = require("./utility/common");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors({origin:'*'}));
 const AuthRouter = require("./routers/Auth");
 
+
 app.use("/auth", AuthRouter);
 
 app.use((req, res) => {
-    res.status(404).send(failure("Page Not Found"));
+    return response(res, 404, "Not Found");
   });
 
 connectDB(() => {
-  app.listen(8000, () => {
+  app.listen(process.env.PORT, () => {
     console.log('Example app listening on port 8000!');
 });
 });
--- routers/Auth.js
+++ routers/Auth.js
@@ -4,7 +4,7 @@ const AuthController = require("../controller/AuthController");
 const limiter = require("../middleware/limiter");
 const authvalidator = require("../middleware/Validation");
 
- routes.post("/signup",authvalidator.signUp,AuthController.signUp);
+routes.post("/signup",authvalidator.signUp,AuthController.signUp);
 routes.post("/login",limiter,authvalidator.login,AuthController.login);
-
+routes.post("/verifyemail/:id",AuthController.verifyEmail);
 module.exports = routes;
