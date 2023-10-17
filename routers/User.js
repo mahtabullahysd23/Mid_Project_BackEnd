95f2bdc9e5b22e3a99ff3b7b37c2597c918ef258
mahtabullahysd23 <mahtab.ullah@bjitacademy.com>
2023-10-17 16:43:34 +0600

    user profile added]

--- routers/User.js
+++ routers/User.js
@@ -2,12 +2,20 @@ const express = require("express");
 const routes = express();
 const UserController = require("../controller/UserController");
 const {isValidAdmin} = require("../middleware/auth");
+const {isValidUser} = require("../middleware/auth");
 const {userValidator,pagelimitValidator} = require("../middleware/Validation");
 
 
+
 routes.get("/all",isValidAdmin,pagelimitValidator.pageLimit,UserController.getAllUser);
-routes.get("/:id",isValidAdmin,UserController.getUserById);
+routes.get("/admin/:id",isValidAdmin,UserController.getUserById);
 routes.patch("/update/:id",isValidAdmin,userValidator.update,UserController.updateUser);
 routes.delete("/delete/:id",isValidAdmin,UserController.deleteUser);
+routes.get("/my-profile",isValidUser,UserController.getMyProfile);
+routes.post("/update-profile",isValidUser,userValidator.updateProfile,UserController.updateProfile);
+
+
+
+
 
 module.exports = routes;
